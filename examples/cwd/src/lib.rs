7d76b549e0813e1aa08af77160a8ff54dfd96bdf
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-04-14 18:42:35 -0700

    Print a newline

--- examples/cwd/src/lib.rs
+++ examples/cwd/src/lib.rs
@@ -1,11 +1,12 @@
-use js_sys::JsString;
+use js_sys::Array;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(start)]
 pub fn main() -> Result<(), JsValue> {
-    web_sys::console::log_1(&JsString::concat(
-        &"[examples/cwd]: ".into(),
-        &node_sys::process.cwd(),
-    ));
+    let msg = Array::new();
+    msg.push(&"[examples/cwd]:".into());
+    msg.push(&node_sys::process.cwd());
+    msg.push(&"\n".into());
+    web_sys::console::log(&msg);
     Ok(())
 }
