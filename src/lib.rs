2da08e99bc1d24a69b78ffbf66d2594427cbcf5f
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-04-15 20:11:27 -0700

    Remove event_emitters.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-pub(crate) mod event_emitter;
 pub(crate) mod process;
 
-pub use event_emitter::EventEmitter;
 pub use process::{process, Process};
--- src/process.rs
+++ src/process.rs
@@ -1,10 +1,8 @@
-use crate::event_emitter::EventEmitter;
 use js_sys::JsString;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 extern "C" {
-    #[wasm_bindgen(extends = EventEmitter)]
     pub type Process;
 
     pub static process: Process;
