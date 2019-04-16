c1ea6a434c0f553017e3d426e671378dbcd30263
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-04-15 21:21:30 -0700

    Add some bindings for "os"

--- src/os.rs
+++ src/os.rs
@@ -1,4 +1,11 @@
+use js_sys::JsString;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(module = os)]
-extern "C" {}
+extern "C" {
+    pub fn arch() -> JsString;
+
+    pub fn tmpdir() -> JsString;
+
+    pub static EOL: JsString;
+}
