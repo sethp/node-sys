813a98a34b70ef3d9bacd044eccd3d5cfaa328f8
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-05-11 18:37:46 -0700

    Start adding bindings for "assert"

--- src/assert.rs
+++ src/assert.rs
@@ -1,4 +1,38 @@
+use js_sys::{Boolean, Error, JsString, Object};
 use wasm_bindgen::prelude::*;
 
+// AssertionError
 #[wasm_bindgen(module = assert)]
-extern "C" {}
+extern "C" {
+    #[wasm_bindgen(extends = Object, extends = Error)]
+    #[derive(Clone, Debug, PartialEq, Eq)]
+    pub type AssertionError;
+
+    #[wasm_bindgen(constructor)]
+    pub fn new(options: &Object) -> AssertionError;
+
+    #[wasm_bindgen(method, getter = actual)]
+    pub fn actual(this: &AssertionError) -> JsValue;
+
+    #[wasm_bindgen(method, getter = code)]
+    pub fn code(this: &AssertionError) -> JsString;
+
+    #[wasm_bindgen(method, getter = expected)]
+    pub fn expected(this: &AssertionError) -> JsValue;
+
+    #[wasm_bindgen(method, getter = generatedMessage)]
+    pub fn generated_message(this: &AssertionError) -> Boolean;
+
+    #[wasm_bindgen(method, getter = operator)]
+    pub fn operator(this: &AssertionError) -> JsString;
+}
+
+#[wasm_bindgen(module = assert)]
+extern "C" {
+    #[wasm_bindgen(catch, js_name = "strictEqual")]
+    pub fn strict_equal(
+        actual: &JsValue,
+        expected: &JsValue,
+        message: Option<Error>,
+    ) -> Result<(), JsValue>;
+}
