3f78e5dd73b78fd528bb46fb242000b89ee5cd6a
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-05-11 18:39:39 -0700

    Add a test for AssertionError

---
+++ tests/assert.rs
@@ -0,0 +1,30 @@
+#![cfg(feature = "assert")]
+
+use js_sys::{Error, Object, Reflect};
+use node_sys::assert::{self, AssertionError};
+use wasm_bindgen::JsCast;
+use wasm_bindgen_test::*;
+
+#[wasm_bindgen_test]
+fn assertion_error_new() {
+    // manual error
+    let manual = assert::AssertionError::new(&{
+        use js_sys::Reflect::set;
+        let opts = Object::new();
+        set(&opts, &"actual".into(), &18u32.into()).unwrap();
+        set(&opts, &"expected".into(), &29u32.into()).unwrap();
+        set(&opts, &"operator".into(), &"strictEqual".into()).unwrap();
+        opts.into()
+    });
+    // thrown error
+    if let Err(thrown) = assert::strict_equal(&18u32.into(), &29u32.into(), None) {
+        let thrown = thrown.unchecked_into::<AssertionError>();
+        assert_eq!(manual.operator(), thrown.operator());
+        assert_eq!(manual.actual(), thrown.actual());
+        assert_eq!(manual.expected(), thrown.expected());
+        assert_eq!(
+            manual.unchecked_into::<Error>().message(),
+            thrown.unchecked_into::<Error>().message()
+        );
+    }
+}
