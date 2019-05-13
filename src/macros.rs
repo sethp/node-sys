8688d2502972addded88d9fd8ebff566e1f68608
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-05-12 21:02:03 -0700

    Simplify options! macro rules

--- src/macros.rs
+++ src/macros.rs
@@ -3,34 +3,24 @@ macro_rules! options {
     ($($rest:tt)*) => {{
         use js_sys::{Object, Reflect};
         let this = Object::new();
-        $crate::__options_rest!(this, $($rest)*)
+        $crate::__options_rest!(this , $($rest)*)
     }};
 }
 
 #[doc(hidden)]
 #[macro_export]
 macro_rules! __options_rest {
-    ($this:tt , $key:ident : $val:expr , $($rest:tt)*) => {
+    { $this:tt , $key:ident $($rest:tt)* } => {
         {
-            $crate::__options_str_key_val!($this, $key : $val, $($rest)*)
+            $crate::__options_str_key_val!($this , $key $($rest)*)
         }
     };
-    ($this:tt , $key:tt : $val:expr , $($rest:tt)*) => {
+    { $this:tt , $key:tt $($rest:tt)* } => {
         {
-            $crate::__options_raw_key_val!($this, $key : $val, $($rest)*)
+            $crate::__options_raw_key_val!($this , $key $($rest)*)
         }
     };
-    ($this:tt , $key:ident : $val:expr) => {
-        {
-            $crate::__options_str_key_val!($this, $key : $val,)
-        }
-    };
-    ($this:tt , $key:tt : $val:expr) => {
-        {
-            $crate::__options_raw_key_val!($this, $key : $val,)
-        }
-    };
-    ($this:tt ,) => {
+    { $this:tt , } => {
         {
             $this.into()
         }
@@ -40,17 +30,17 @@ macro_rules! __options_rest {
 #[doc(hidden)]
 #[macro_export]
 macro_rules! __options_raw_key_val {
-    ($this:tt , $key:tt : $val:tt , $($rest:tt)*) => {{
+    { $this:tt , $key:tt : $val:tt , $($rest:tt)* } => {{
         Reflect::set(&$this, &$key.into(), &$val.into()).unwrap();
-        $crate::__options_rest!($this, $($rest)*)
+        $crate::__options_rest!($this , $($rest)*)
     }};
 }
 
 #[doc(hidden)]
 #[macro_export]
 macro_rules! __options_str_key_val {
-    ($this:tt , $key:ident : $val:tt , $($rest:tt)*) => {{
+    { $this:tt , $key:ident : $val:tt , $($rest:tt)* } => {{
         Reflect::set(&$this, &stringify!($key).into(), &$val.into()).unwrap();
-        $crate::__options_rest!($this, $($rest)*)
+        $crate::__options_rest!($this , $($rest)*)
     }};
 }
