cf5bfcd828379491c46a9f14e4484e9aa33a3ced
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-05-12 21:29:29 -0700

    Fix macro to handle trailing commas again

--- src/macros.rs
+++ src/macros.rs
@@ -3,24 +3,24 @@ macro_rules! options {
     ($($rest:tt)*) => {{
         use js_sys::{Object, Reflect};
         let this = Object::new();
-        $crate::__options_rest!(this , $($rest)*)
+        $crate::__options_rest!(this | , $($rest)*)
     }};
 }
 
 #[doc(hidden)]
 #[macro_export]
 macro_rules! __options_rest {
-    { $this:tt , $key:ident $($rest:tt)* } => {
+    { $this:tt | , $key:ident $($rest:tt)* } => {
         {
-            $crate::__options_key_val!($this , (stringify!($key)) $($rest)*)
+            $crate::__options_key_val!($this | (stringify!($key)) $($rest)*)
         }
     };
-    { $this:tt , $key:tt $($rest:tt)* } => {
+    { $this:tt | , $key:tt $($rest:tt)* } => {
         {
-            $crate::__options_key_val!($this , $key $($rest)*)
+            $crate::__options_key_val!($this | $key $($rest)*)
         }
     };
-    { $this:tt , } => {
+    { $this:tt | $(,)? } => {
         {
             $this.into()
         }
@@ -30,8 +30,8 @@ macro_rules! __options_rest {
 #[doc(hidden)]
 #[macro_export]
 macro_rules! __options_key_val {
-    { $this:tt , $key:tt : $val:tt , $($rest:tt)* } => {{
+    { $this:tt | $key:tt : $val:tt $($rest:tt)* } => {{
         Reflect::set(&$this, &$key.into(), &$val.into()).unwrap();
-        $crate::__options_rest!($this , $($rest)*)
+        $crate::__options_rest!($this | $($rest)*)
     }};
 }
