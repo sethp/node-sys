74ebfda5a8395e08f53b4eb6c4afb5b128c8c10b
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-05-17 18:54:11 -0700

    $this should only ever be an ident

--- src/macros.rs
+++ src/macros.rs
@@ -10,17 +10,17 @@ macro_rules! options {
 #[doc(hidden)]
 #[macro_export]
 macro_rules! __options_rest {
-    { $this:tt | , $key:ident $($rest:tt)* } => {
+    { $this:ident | , $key:ident $($rest:tt)* } => {
         {
             $crate::__options_key_val!($this | (stringify!($key)) $($rest)*)
         }
     };
-    { $this:tt | , $key:tt $($rest:tt)* } => {
+    { $this:ident | , $key:tt $($rest:tt)* } => {
         {
             $crate::__options_key_val!($this | $key $($rest)*)
         }
     };
-    { $this:tt | $(,)? } => {
+    { $this:ident | $(,)? } => {
         {
             $this.into()
         }
@@ -30,7 +30,7 @@ macro_rules! __options_rest {
 #[doc(hidden)]
 #[macro_export]
 macro_rules! __options_key_val {
-    { $this:tt | $key:tt : $val:tt $($rest:tt)* } => {{
+    { $this:ident | $key:tt : $val:tt $($rest:tt)* } => {{
         Reflect::set(&$this, &$key.into(), &$val.into()).unwrap();
         $crate::__options_rest!($this | $($rest)*)
     }};
