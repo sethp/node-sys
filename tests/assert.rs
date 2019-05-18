1ead8e441056279bf9ba822cf3a5bca8ad54b78c
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-05-18 13:55:29 -0700

    Adjust test to match docs

--- tests/assert.rs
+++ tests/assert.rs
@@ -3,7 +3,7 @@
 use js_sys::Error;
 use node_sys::{
     assert::{self, AssertionError},
-    options,
+    options, Cast,
 };
 use wasm_bindgen::JsCast;
 use wasm_bindgen_test::*;
@@ -11,20 +11,30 @@ use wasm_bindgen_test::*;
 #[wasm_bindgen_test]
 fn assertion_error_new() {
     // manual error
-    let manual = assert::AssertionError::new(&options! {
-        actual: 18u32,
-        expected: 29u32,
+    let manual = AssertionError::new(&options! {
+        actual: 1u32,
+        expected: 2u32,
         operator: "strictEqual",
     });
     // thrown error
-    if let Err(thrown) = assert::strict_equal(&18u32.into(), &29u32.into()) {
+    if let Err(thrown) = assert::strict_equal(&1u32.into(), &2u32.into()) {
+        assert!(thrown.is_instance_of::<AssertionError>());
         let thrown = thrown.unchecked_into::<AssertionError>();
-        assert_eq!(manual.operator(), thrown.operator());
-        assert_eq!(manual.actual(), thrown.actual());
-        assert_eq!(manual.expected(), thrown.expected());
+
+        assert_eq!(thrown.cast::<Error>().name(), "AssertionError");
+        assert_eq!(thrown.actual(), 1);
+        assert_eq!(thrown.expected(), 2);
+        assert_eq!(thrown.code(), "ERR_ASSERTION");
+        assert_eq!(thrown.operator(), "strictEqual");
+        assert_eq!(thrown.generated_message(), true);
+
         assert_eq!(
-            manual.unchecked_into::<Error>().message(),
-            thrown.unchecked_into::<Error>().message()
+            thrown.cast::<Error>().message(),
+            manual.cast::<Error>().message(),
         );
+        assert_eq!(thrown.cast::<Error>().name(), manual.cast::<Error>().name());
+        assert_eq!(thrown.actual(), manual.actual());
+        assert_eq!(thrown.expected(), manual.expected());
+        assert_eq!(thrown.operator(), manual.operator());
     }
 }
