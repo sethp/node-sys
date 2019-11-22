ddd8418c5becfffea7a391d5bcef45a0c5c1ce69
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-22 16:17:38 -0700

    Test for CreateHookCallbacks::new

--- tests/callable.rs
+++ tests/callable.rs
@@ -47,3 +47,39 @@ mod assert {
         }
     }
 }
+
+mod async_hooks {
+    use node_sys::async_hooks;
+    use wasm_bindgen_test::*;
+
+    mod helper {
+        use node_sys::async_hooks;
+        use wasm_bindgen::{prelude::*, JsCast};
+
+        pub fn create_hook_callbacks() -> async_hooks::CreateHookCallbacks {
+            let init = Closure::wrap(Box::new(move || {}) as Box<dyn Fn()>);
+            let before = Closure::wrap(Box::new(move || {}) as Box<dyn Fn()>);
+            let after = Closure::wrap(Box::new(move || {}) as Box<dyn Fn()>);
+            let destroy = Closure::wrap(Box::new(move || {}) as Box<dyn Fn()>);
+            let promise_resolve = Closure::wrap(Box::new(move || {}) as Box<dyn Fn()>);
+            let callbacks = async_hooks::CreateHookCallbacks::new(
+                init.as_ref().unchecked_ref(),
+                before.as_ref().unchecked_ref(),
+                after.as_ref().unchecked_ref(),
+                destroy.as_ref().unchecked_ref(),
+                promise_resolve.as_ref().unchecked_ref(),
+            );
+            init.forget();
+            before.forget();
+            after.forget();
+            destroy.forget();
+            promise_resolve.forget();
+            callbacks
+        }
+    }
+
+    #[wasm_bindgen_test]
+    fn create_hook_callbacks_new() {
+        helper::create_hook_callbacks();
+    }
+}
