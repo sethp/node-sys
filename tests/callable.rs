a0e374fe48d13bbcc3d29fd7f2dde08f47ce4eb8
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-22 14:42:13 -0700

    Callable test for AssertionError::new()

--- tests/callable.rs
+++ tests/callable.rs
@@ -17,4 +17,20 @@ mod assert {
         }
         test().unwrap()
     }
+
+    #[wasm_bindgen_test]
+    fn assertion_error_new() {
+        fn test() -> Result<(), JsValue> {
+            let message = Default::default();
+            let actual = 0u32.into();
+            let expected = 1u32.into();
+            let operator = "strictEqual".into();
+            let options = node_sys::assert::AssertionErrorOptions::new(message, actual, expected, operator)?;
+            let error = node_sys::assert::AssertionError::new(options);
+            let message = error.message();
+            assert_eq!(message, "Expected values to be strictly equal:\n\n0 !== 1\n");
+            Ok(())
+        }
+        test().unwrap()
+    }
 }
