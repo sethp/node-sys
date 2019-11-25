70b24adc7740c5e83a9cdf887ac1d53465eda989
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-25 10:54:58 -0700

    Add stub for process module

--- src/module/process.rs
+++ src/module/process.rs
@@ -1,1 +1,11 @@
+use crate::module::events::EventEmitter;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern {
+    #[wasm_bindgen(extends = EventEmitter)]
+    pub type Process;
+
+    pub static process: Process;
+}
 
