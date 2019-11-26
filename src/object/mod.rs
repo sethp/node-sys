ab1de11e6c148a621dd568240f668033015db0d2
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-25 20:29:11 -0700

    Bindings for NodeRequireFunction object

--- src/object/mod.rs
+++ src/object/mod.rs
@@ -3,6 +3,7 @@ pub(crate) mod cpu_usage;
 pub(crate) mod domain;
 pub(crate) mod hr_time;
 pub(crate) mod memory_usage;
+pub(crate) mod node_require_function;
 pub(crate) mod timer;
 
 pub use buffer::*;
@@ -10,4 +11,5 @@ pub use cpu_usage::*;
 pub use domain::*;
 pub use hr_time::*;
 pub use memory_usage::*;
+pub use node_require_function::*;
 pub use timer::*;
