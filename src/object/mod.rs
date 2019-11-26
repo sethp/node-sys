4f231fe0f4281c1fef2b0b7a81e1a36b7cf5d516
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-25 20:27:50 -0700

    Bindings for HRTime object

--- src/object/mod.rs
+++ src/object/mod.rs
@@ -1,9 +1,11 @@
 pub(crate) mod buffer;
 pub(crate) mod cpu_usage;
 pub(crate) mod domain;
+pub(crate) mod hr_time;
 pub(crate) mod timer;
 
 pub use buffer::*;
 pub use cpu_usage::*;
 pub use domain::*;
+pub use hr_time::*;
 pub use timer::*;
