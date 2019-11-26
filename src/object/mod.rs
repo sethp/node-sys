c545de2dd2470f043373e9cd071e056b03e8a1d7
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-25 20:29:46 -0700

    Bindings for NodeModule object

--- src/object/mod.rs
+++ src/object/mod.rs
@@ -3,6 +3,7 @@ pub(crate) mod cpu_usage;
 pub(crate) mod domain;
 pub(crate) mod hr_time;
 pub(crate) mod memory_usage;
+pub(crate) mod node_module;
 pub(crate) mod node_require_function;
 pub(crate) mod timer;
 
@@ -11,5 +12,6 @@ pub use cpu_usage::*;
 pub use domain::*;
 pub use hr_time::*;
 pub use memory_usage::*;
+pub use node_module::*;
 pub use node_require_function::*;
 pub use timer::*;
---
+++ src/object/node_module.rs
@@ -0,0 +1,33 @@
+use crate::object::NodeRequireFunction;
+use js_sys::{Array, JsString};
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern {
+    #[derive(Clone, Debug, PartialEq)]
+    pub type NodeModule;
+
+    #[wasm_bindgen(method, getter)]
+    pub fn children(this: &NodeModule) -> Array;
+
+    #[wasm_bindgen(method, getter)]
+    pub fn exports(this: &NodeModule) -> JsValue;
+
+    #[wasm_bindgen(method, getter)]
+    pub fn filename(this: &NodeModule) -> JsString;
+
+    #[wasm_bindgen(method, getter)]
+    pub fn id(this: &NodeModule) -> JsString;
+
+    #[wasm_bindgen(method, getter)]
+    pub fn loaded(this: &NodeModule) -> bool;
+
+    #[wasm_bindgen(method, getter)]
+    pub fn parent(this: &NodeModule) -> Option<NodeModule>;
+
+    #[wasm_bindgen(method, getter)]
+    pub fn paths(this: &NodeModule) -> Array;
+
+    #[wasm_bindgen(method, getter)]
+    pub fn require(this: &NodeModule) -> NodeRequireFunction;
+}
