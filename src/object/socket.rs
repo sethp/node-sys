bac01211f48aea0bbdb7e43f6d32797cb8001d5f
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-25 21:26:18 -0700

    Bindings for Socket object

---
+++ src/object/socket.rs
@@ -0,0 +1,11 @@
+use crate::object::ReadWriteStream;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern {
+    #[wasm_bindgen(extends = ReadWriteStream)]
+    pub type Socket;
+
+    #[wasm_bindgen(method, getter, js_name = "isTTY")]
+    pub fn is_tty(this: &Socket) -> bool;
+}
