132a2ef45422b1c3f9f535730cd6176ecb5cec19
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-28 12:19:09 -0700

    Test for events::once

--- tests/callable.rs
+++ tests/callable.rs
@@ -89,6 +89,25 @@ mod async_hooks {
     }
 }
 
+mod events {
+    use node_sys::{events, events::EventEmitter, process};
+    use wasm_bindgen::{prelude::*, JsCast};
+    use wasm_bindgen_futures::JsFuture;
+    use wasm_bindgen_test::*;
+
+    #[wasm_bindgen_test]
+    async fn once() {
+        let emitter = EventEmitter::new();
+        let promise = events::once(&emitter, "event");
+        let clo = Closure::wrap(Box::new(move || {
+            emitter.emit("event", Box::new([42.into()]));
+        }) as Box<dyn Fn()>);
+        process.next_tick(clo.as_ref().unchecked_ref(), Box::new([0.into()]));
+        clo.forget();
+        JsFuture::from(promise).await.unwrap_throw();
+    }
+}
+
 mod process {
     use node_sys::process;
     use wasm_bindgen_test::*;
