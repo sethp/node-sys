02662fc6805636c79db042d68cd2c89cb270f409
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-28 12:15:43 -0700

    Fix events exports

--- src/module/events.rs
+++ src/module/events.rs
@@ -5,13 +5,8 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(module = "events")]
 extern {
-    pub type Events;
-
-    pub static events: Events;
-
     /// Creates a `Promise` that is fulfilled when the `EventEmitter` emits the given event or that
     /// is rejected when the `EventEmitter` emits `"error"`. The `Promise` will resolve with an
     /// array of all the arguments emitted to the given event.
-    #[wasm_bindgen]
-    pub fn once(event_emitter: &Events, emitter: &EventEmitter, name: &str) -> Promise;
+    pub fn once(emitter: &EventEmitter, name: &str) -> Promise;
 }
