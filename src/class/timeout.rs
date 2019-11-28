38491b243b6af77b8062658644c0d9d9607851c5
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-27 21:07:22 -0700

    Derive impls for Timeout

--- src/class/timeout.rs
+++ src/class/timeout.rs
@@ -1,8 +1,10 @@
 use crate::interface::Timer;
+use js_sys::Object;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 extern {
-    #[wasm_bindgen(extends = Timer)]
+    #[wasm_bindgen(extends = Object, extends = Timer)]
+    #[derive(Clone, Debug, Eq, PartialEq)]
     pub type Timeout;
 }
