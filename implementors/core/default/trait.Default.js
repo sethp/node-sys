150c85a2a50af96980fbbb0b4c23e1e1735a75ac
darinmorrison <darinmorrison@users.noreply.github.com>
2019-11-30 22:21:21 +0000

    Automated deployment: Sat Nov 30 22:21:21 UTC 2019 d5e27bfa3897f8ed9e3a99c15ea978af70a08615

---
+++ implementors/core/default/trait.Default.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["node_sys"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"node_sys/struct.PipeOptions.html\" title=\"struct node_sys::PipeOptions\">PipeOptions</a>",synthetic:false,types:["node_sys::interface::pipe_options::PipeOptions"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
