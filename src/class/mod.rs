3ca8a83239ebd977dfbb6dc89f915b56907857ba
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-30 15:07:58 -0700

    Bindings for ReadStream class

--- src/class/mod.rs
+++ src/class/mod.rs
@@ -2,10 +2,12 @@ pub(crate) mod buffer;
 pub(crate) mod event_emitter;
 pub(crate) mod immediate;
 pub(crate) mod module;
+pub(crate) mod read_stream;
 pub(crate) mod timeout;
 
 pub use buffer::*;
 pub use event_emitter::*;
 pub use immediate::*;
 pub use module::*;
+pub use read_stream::*;
 pub use timeout::*;
---
+++ src/class/read_stream.rs
@@ -0,0 +1,70 @@
+use crate::interface::ReadableStream;
+use js_sys::Function;
+use wasm_bindgen::{prelude::*, JsCast};
+
+#[wasm_bindgen(module = "events")]
+extern {
+    #[wasm_bindgen(extends = ReadableStream)]
+    #[derive(Clone, Debug, Eq, PartialEq)]
+    pub type ReadStream;
+
+    //******************//
+    // Instance Methods //
+    //******************//
+
+    #[wasm_bindgen(method)]
+    pub fn close(this: &ReadStream);
+
+    #[wasm_bindgen(method, js_name = "bytesRead")]
+    pub fn bytes_read(this: &ReadStream) -> usize;
+
+    #[wasm_bindgen(method)]
+    pub fn path(this: &ReadStream) -> JsValue; // Buffer | string
+}
+
+//******************************//
+// Instance Methods (Overloads) //
+//******************************//
+
+#[wasm_bindgen]
+impl ReadStream {
+    pub fn add_listener_with_open(&self, listener: &Function) -> ReadStream {
+        self.add_listener("open", listener).unchecked_into()
+    }
+
+    pub fn add_listener_with_close(&self, listener: &Function) -> ReadStream {
+        self.add_listener("close", listener).unchecked_into()
+    }
+
+    pub fn on_with_open(&self, listener: &Function) -> ReadStream {
+        self.on("open", listener).unchecked_into()
+    }
+
+    pub fn on_with_close(&self, listener: &Function) -> ReadStream {
+        self.on("close", listener).unchecked_into()
+    }
+
+    pub fn once_with_open(&self, listener: &Function) -> ReadStream {
+        self.once("open", listener).unchecked_into()
+    }
+
+    pub fn once_with_close(&self, listener: &Function) -> ReadStream {
+        self.once("close", listener).unchecked_into()
+    }
+
+    pub fn prepend_listener_with_open(&self, listener: &Function) -> ReadStream {
+        self.prepend_listener("open", listener).unchecked_into()
+    }
+
+    pub fn prepend_listener_with_close(&self, listener: &Function) -> ReadStream {
+        self.prepend_listener("close", listener).unchecked_into()
+    }
+
+    pub fn prepend_once_listener_with_open(&self, listener: &Function) -> ReadStream {
+        self.prepend_once_listener("open", listener).unchecked_into()
+    }
+
+    pub fn prepend_once_listener_with_close(&self, listener: &Function) -> ReadStream {
+        self.prepend_once_listener("close", listener).unchecked_into()
+    }
+}
