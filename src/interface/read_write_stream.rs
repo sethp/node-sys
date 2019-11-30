65039f1ba4279302547e0b6116dd9e8b3319d5ae
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-30 15:07:00 -0700

    Derive impls for ReadWriteStream

--- src/interface/read_write_stream.rs
+++ src/interface/read_write_stream.rs
@@ -4,5 +4,6 @@ use wasm_bindgen::prelude::*;
 #[wasm_bindgen]
 extern {
     #[wasm_bindgen(extends = ReadableStream, extends = WritableStream)]
+    #[derive(Clone, Debug, Eq, PartialEq)]
     pub type ReadWriteStream;
 }
