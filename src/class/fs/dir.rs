18c0b57be646408cbb335ca3e851e31f9bef8065
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-08 14:28:47 -0700

    Bindings for fs.Dir class

--- src/class/fs/dir.rs
+++ src/class/fs/dir.rs
@@ -1,9 +1,33 @@
-use js_sys::Object;
+use crate::{class::fs::Dirent, interface::AsyncIterator};
+use js_sys::{Function, JsString, Object, Promise};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(module = "fs")]
 extern {
-    #[wasm_bindgen(extends = Object)]
+    #[wasm_bindgen(extends = Object, extends = AsyncIterator)]
     #[derive(Clone, Debug, Eq, PartialEq)]
     pub type Dir;
+
+    //******************//
+    // Instance Methods //
+    //******************//
+
+    #[wasm_bindgen(method)]
+    pub fn close(this: &Dir, callback: Option<&Function>) -> Promise;
+
+    #[wasm_bindgen(method, js_name = "closeSync")]
+    pub fn close_sync(this: &Dir);
+
+    #[wasm_bindgen(method)]
+    pub fn read(this: &Dir) -> Promise;
+
+    #[wasm_bindgen(method, js_name = "readSync")]
+    pub fn read_sync(this: &Dir) -> Dirent;
+
+    //*********************//
+    // Instance Properties //
+    //*********************//
+
+    #[wasm_bindgen(method, getter)]
+    pub fn path(this: &Dir) -> JsString;
 }
