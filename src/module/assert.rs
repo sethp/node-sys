ebc6579f9717deaf385de162ccc686418ad7f46a
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-07 21:56:35 -0700

    Bindings for assert module

--- src/module/assert.rs
+++ src/module/assert.rs
@@ -1,5 +1,5 @@
 pub use crate::interface::assertion_error_options::AssertionErrorOptions;
-use js_sys::{Error, JsString};
+use js_sys::{Error, Function, JsString, Object, Promise};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(module = "assert")]
@@ -10,6 +10,59 @@ extern {
     #[wasm_bindgen(constructor)]
     pub fn new(options: AssertionErrorOptions) -> AssertionError;
 
+    #[must_use]
     #[wasm_bindgen(catch, js_name = "deepStrictEqual")]
     pub fn deep_strict_equal(actual: &JsValue, expected: &JsValue, message: Option<&JsString>) -> Result<(), JsValue>;
+
+    #[must_use]
+    #[wasm_bindgen(js_name = "doesNotReject")]
+    pub fn does_not_reject_function(block: &Function, error: Option<&Object>, message: Option<&JsString>) -> Promise;
+
+    #[must_use]
+    #[wasm_bindgen(js_name = "doesNotReject")]
+    pub fn does_not_reject_promise(block: &Promise, error: Option<&Object>, message: Option<&JsString>) -> Promise;
+
+    #[must_use]
+    #[wasm_bindgen(catch, js_name = "doesNotThrow")]
+    pub fn does_not_throw(block: &Function, error: Option<&Object>, message: Option<&JsString>) -> Result<(), JsValue>;
+
+    #[must_use]
+    #[wasm_bindgen(catch)]
+    pub fn fail(message: Option<&JsString>) -> Result<(), JsValue>;
+
+    #[must_use]
+    #[wasm_bindgen(catch, js_name = "ifError")]
+    pub fn if_error(value: &JsValue) -> Result<(), JsValue>;
+
+    #[must_use]
+    #[wasm_bindgen(catch, js_name = "notDeepStrictEqual")]
+    pub fn not_deep_strict_equal(
+        actual: &JsValue,
+        expected: &JsValue,
+        message: Option<&JsString>,
+    ) -> Result<(), JsValue>;
+
+    #[must_use]
+    #[wasm_bindgen(catch, js_name = "notStrictEqual")]
+    pub fn not_strict_equal(actual: &JsValue, expected: &JsValue, message: Option<&JsString>) -> Result<(), JsValue>;
+
+    #[must_use]
+    #[wasm_bindgen(catch)]
+    pub fn ok(value: &JsValue, message: Option<&JsString>) -> Result<(), JsValue>;
+
+    #[must_use]
+    #[wasm_bindgen(js_name = "rejects")]
+    pub fn rejects_function(block: &Function, error: Option<&Object>, message: Option<&JsString>) -> Promise;
+
+    #[must_use]
+    #[wasm_bindgen(js_name = "rejects")]
+    pub fn rejects_promise(block: &Promise, error: Option<&Object>, message: Option<&JsString>) -> Promise;
+
+    #[must_use]
+    #[wasm_bindgen(catch, js_name = "strictEqual")]
+    pub fn strict_equal(actual: &JsValue, expected: &JsValue, message: Option<&JsString>) -> Result<(), JsValue>;
+
+    #[must_use]
+    #[wasm_bindgen(catch)]
+    pub fn throws(block: &Function, error: Option<&Object>, message: Option<&JsString>) -> Result<(), JsValue>;
 }
