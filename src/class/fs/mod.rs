f60a3cc87eea512891e2a963ec5153a7050f92bf
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-08 12:42:23 -0700

    Stub for fs.Stats class

--- src/class/fs/mod.rs
+++ src/class/fs/mod.rs
@@ -2,10 +2,12 @@ pub(crate) mod dir;
 pub(crate) mod dirent;
 pub(crate) mod fs_watcher;
 pub(crate) mod read_stream;
+pub(crate) mod stats;
 pub(crate) mod write_stream;
 
 pub use dir::*;
 pub use dirent::*;
 pub use fs_watcher::*;
 pub use read_stream::*;
+pub use stats::*;
 pub use write_stream::*;
