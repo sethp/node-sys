cb67cd94e86ab5681fac9426ee6851a2658d0260
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-08 05:44:32 -0700

    Bindings for Buffer static methods and properties

--- src/class/buffer.rs
+++ src/class/buffer.rs
@@ -1,4 +1,4 @@
-use js_sys::Uint8Array;
+use js_sys::{Array, ArrayBuffer, JsString, Uint8Array};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -6,4 +6,40 @@ extern {
     #[wasm_bindgen(extends = Uint8Array)]
     #[derive(Clone, Debug)]
     pub type Buffer;
+
+    #[wasm_bindgen(static_method_of = Buffer)]
+    pub fn alloc(size: usize, fill: Option<&Buffer>, encoding: Option<&JsString>) -> Buffer;
+
+    #[wasm_bindgen(static_method_of = Buffer, js_name = "allocUnsafe")]
+    pub fn alloc_unsafe(size: usize) -> Buffer;
+
+    #[wasm_bindgen(static_method_of = Buffer, js_name = "byteLength")]
+    pub fn byte_length(string: &JsString, encoding: Option<&JsString>) -> u32;
+
+    #[wasm_bindgen(static_method_of = Buffer)]
+    pub fn compare(buf1: &Buffer, buf2: &Buffer) -> i32;
+
+    #[wasm_bindgen(static_method_of = Buffer)]
+    pub fn concat(list: &Array, total_length: Option<u32>) -> Buffer;
+
+    #[wasm_bindgen(static_method_of = Buffer, js_name = "isBuffer")]
+    pub fn is_buffer(value: &JsValue) -> bool;
+
+    #[wasm_bindgen(static_method_of = Buffer, js_name = "isEncoding")]
+    pub fn is_encoding(encoding: &JsString) -> bool;
+
+    #[wasm_bindgen(static_method_of = Buffer, js_name = "from")]
+    pub fn from_array(array: &Array) -> Buffer;
+
+    #[wasm_bindgen(static_method_of = Buffer, js_name = "from")]
+    pub fn from_array_buffer(buffer: &ArrayBuffer, byte_offset: Option<u32>, length: Option<u32>) -> Buffer;
+
+    #[wasm_bindgen(static_method_of = Buffer, js_name = "from")]
+    pub fn from_array_uint8(array: &Uint8Array) -> Buffer;
+
+    #[wasm_bindgen(static_method_of = Buffer, js_name = "from")]
+    pub fn from_string(string: &JsString, encoding: Option<&JsString>) -> Buffer;
+
+    #[wasm_bindgen(static_method_of = Buffer, getter, js_name = "poolSize")]
+    pub fn pool_size() -> usize;
 }
