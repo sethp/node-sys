6c8ba52fca36d7e92b31d77b273a86a6cc10805d
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-08 12:40:51 -0700

    Stub for fs.Dir class

---
+++ src/class/fs/dir.rs
@@ -0,0 +1,9 @@
+use js_sys::Object;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen(module = "fs")]
+extern {
+    #[wasm_bindgen(extends = Object)]
+    #[derive(Clone, Debug, Eq, PartialEq)]
+    pub type Dir;
+}
--- src/class/fs/mod.rs
+++ src/class/fs/mod.rs
@@ -1,5 +1,7 @@
+pub(crate) mod dir;
 pub(crate) mod read_stream;
 pub(crate) mod write_stream;
 
+pub use dir::*;
 pub use read_stream::*;
 pub use write_stream::*;
