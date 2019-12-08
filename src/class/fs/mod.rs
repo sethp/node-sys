0489545a6bc97bc3cc953cd87bc1726da305e2e9
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-08 12:42:00 -0700

    Stub for fs.FSWatcher class

--- src/class/fs/mod.rs
+++ src/class/fs/mod.rs
@@ -1,9 +1,11 @@
 pub(crate) mod dir;
 pub(crate) mod dirent;
+pub(crate) mod fs_watcher;
 pub(crate) mod read_stream;
 pub(crate) mod write_stream;
 
 pub use dir::*;
 pub use dirent::*;
+pub use fs_watcher::*;
 pub use read_stream::*;
 pub use write_stream::*;
