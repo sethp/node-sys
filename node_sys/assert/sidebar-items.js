957993bdbde5b8ef45c920c49fec0680d863318c
darinmorrison <darinmorrison@users.noreply.github.com>
2019-12-08 00:58:21 +0000

    Automated deployment: Sun Dec  8 00:58:21 UTC 2019 0b74e875d050997061013103d61b0d30385f63d7

--- node_sys/assert/sidebar-items.js
+++ node_sys/assert/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["deep_strict_equal","Tests for deep equality between the actual and expected parameters. \"Deep\" equality means that the enumerable \"own\" properties of child objects are recursively evaluated also by the following rules."]],"struct":[["AssertionError","Indicates the failure of an assertion. All errors thrown by the assert module will be instances of the AssertionError class."],["AssertionErrorOptions",""]]});
+initSidebarItems({"fn":[["deep_strict_equal",""]],"struct":[["AssertionError",""],["AssertionErrorOptions",""]]});
