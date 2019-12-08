59ca7cfe9046c704f39f232fba3a5e780cd7ca84
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-08 14:29:24 -0700

    Bindings for fs.Dirent class

--- src/class/fs/dirent.rs
+++ src/class/fs/dirent.rs
@@ -1,4 +1,4 @@
-use js_sys::Object;
+use js_sys::{JsString, Object};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(module = "fs")]
@@ -6,4 +6,36 @@ extern {
     #[wasm_bindgen(extends = Object)]
     #[derive(Clone, Debug, Eq, PartialEq)]
     pub type Dirent;
+
+    //******************//
+    // Instance Methods //
+    //******************//
+
+    #[wasm_bindgen(method, js_name = "isFile")]
+    pub fn is_file(this: &Dirent) -> bool;
+
+    #[wasm_bindgen(method, js_name = "isDirectory")]
+    pub fn is_directory(this: &Dirent) -> bool;
+
+    #[wasm_bindgen(method, js_name = "isBlockDevice")]
+    pub fn is_block_device(this: &Dirent) -> bool;
+
+    #[wasm_bindgen(method, js_name = "isCharacterDevice")]
+    pub fn is_character_device(this: &Dirent) -> bool;
+
+    #[wasm_bindgen(method, js_name = "isSymbolic")]
+    pub fn is_symbolic_link(this: &Dirent) -> bool;
+
+    #[wasm_bindgen(method, js_name = "isFIFO")]
+    pub fn is_fifo(this: &Dirent) -> bool;
+
+    #[wasm_bindgen(method, js_name = "isSocket")]
+    pub fn is_socket(this: &Dirent) -> bool;
+
+    //*********************//
+    // Instance Properties //
+    //*********************//
+
+    #[wasm_bindgen(method, getter)]
+    pub fn name(this: &Dirent) -> JsString;
 }
