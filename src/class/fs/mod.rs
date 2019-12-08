ddc9d2344a5828c66c59ddd4c9272903856c8372
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-08 12:41:41 -0700

    Stub for fs.Dirent class

--- src/class/fs/mod.rs
+++ src/class/fs/mod.rs
@@ -1,7 +1,9 @@
 pub(crate) mod dir;
+pub(crate) mod dirent;
 pub(crate) mod read_stream;
 pub(crate) mod write_stream;
 
 pub use dir::*;
+pub use dirent::*;
 pub use read_stream::*;
 pub use write_stream::*;
