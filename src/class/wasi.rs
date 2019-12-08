b0a46f7c1358607e835a8592ad60cb338cbf581f
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-08 12:22:22 -0700

    Bindings for WASI class

--- src/class/wasi.rs
+++ src/class/wasi.rs
@@ -1,9 +1,19 @@
-use js_sys::*;
+use crate::interface::WasiOptions;
+use js_sys::{Object, WebAssembly};
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
+#[wasm_bindgen(module = "wasi")]
 extern {
-    #[wasm_bindgen(extends = Object)]
+    #[wasm_bindgen(extends = Object, js_name = "WASI")]
     #[derive(Clone, Debug, Eq, PartialEq)]
-    pub type WASI;
+    pub type Wasi;
+
+    #[wasm_bindgen(constructor)]
+    pub fn new(options: Option<WasiOptions>) -> Wasi;
+
+    #[wasm_bindgen(method)]
+    pub fn start(this: &Wasi, instance: &WebAssembly::Instance);
+
+    #[wasm_bindgen(method, getter, js_name = "wasiImport")]
+    pub fn wasi_import(this: &Wasi) -> Object;
 }
