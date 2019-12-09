046e366cf8c7054b970d8193550676836447e69b
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-08 19:12:02 -0700

    Bind pending property

--- src/class/fs/read_stream.rs
+++ src/class/fs/read_stream.rs
@@ -17,6 +17,9 @@ extern {
 
     #[wasm_bindgen(method, getter)]
     pub fn path(this: &ReadStream) -> JsValue; // Buffer | string
+
+    #[wasm_bindgen(method, getter)]
+    pub fn pending(this: &ReadStream) -> bool;
 }
 
 //******************************//
