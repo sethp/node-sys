11c4c75c6aa4f196d58f1e39fd7b598fde9d555a
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-10 12:50:21 -0700

    Use u32 for mode

--- src/module/fs.rs
+++ src/module/fs.rs
@@ -4,8 +4,8 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(module = "fs")]
 extern {
-    pub fn access(path: &JsString, mode: Option<i32>, callback: &Function);
+    pub fn access(path: &JsString, mode: Option<u32>, callback: &Function);
 
     #[wasm_bindgen(js_name = "accessSync")]
-    pub fn access_sync(path: &JsString, mode: Option<i32>);
+    pub fn access_sync(path: &JsString, mode: Option<u32>);
 }
