dde0a6eefbd34b9d5212ef531ff3624d2ff949f3
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-10 08:06:02 -0700

    Callable tests for Immediate class

--- tests/node.rs
+++ tests/node.rs
@@ -547,6 +547,28 @@ mod class {
             }
         }
     }
+
+    mod immediate {
+        use wasm_bindgen_test::*;
+
+        #[wasm_bindgen_test]
+        pub fn has_ref() {
+            let immediate = crate::helper::immediate::new();
+            immediate.has_ref();
+        }
+
+        #[wasm_bindgen_test]
+        pub fn ref_() {
+            let immediate = crate::helper::immediate::new();
+            immediate.ref_();
+        }
+
+        #[wasm_bindgen_test]
+        pub fn unref() {
+            let immediate = crate::helper::immediate::new();
+            immediate.unref();
+        }
+    }
 }
 
 mod interface {}
