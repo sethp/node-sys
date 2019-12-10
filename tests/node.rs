7211b53ad1922cf36f444604b59e5505ea785bea
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-10 15:58:17 -0700

    Create a helper module for working with temp files

--- tests/node.rs
+++ tests/node.rs
@@ -7,6 +7,31 @@ mod helper {
         }
     }
 
+    pub(crate) mod file {
+        use js_sys::JsString;
+        use node_sys::{fs, os, path};
+
+        pub fn tmpdir() -> JsString {
+            let prefix = &path::join({
+                let mut val = vec![];
+                val.push(os::tmpdir().into());
+                val.push("interfaces-rs-".into());
+                val.into_boxed_slice()
+            });
+            let options = Default::default();
+            fs::mkdtemp_sync(prefix, options)
+        }
+
+        pub fn tmpfile(filename: &str) -> JsString {
+            path::join({
+                let mut val = vec![];
+                val.push(tmpdir().into());
+                val.push(filename.into());
+                val.into_boxed_slice()
+            })
+        }
+    }
+
     pub(crate) mod immediate {
         use node_sys::{timers, Immediate};
         use wasm_bindgen::{prelude::*, JsCast};
