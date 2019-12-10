60c69596f93c73a01f65cde395153f79a5caa5ee
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-10 15:57:32 -0700

    Bindings for fs.appendFile

--- src/module/fs.rs
+++ src/module/fs.rs
@@ -1,4 +1,7 @@
-pub use crate::{class::fs::*, interface::MkdtempSyncOptions};
+pub use crate::{
+    class::{fs::*, Buffer},
+    interface::{AppendFileOptions, MkdtempSyncOptions},
+};
 use js_sys::{Function, JsString};
 use wasm_bindgen::prelude::*;
 
@@ -8,6 +11,9 @@ extern {
 
     #[wasm_bindgen(js_name = "accessSync")]
     pub fn access_sync(path: &JsString, mode: Option<u32>);
+
+    #[wasm_bindgen(js_name = "appendFile")]
+    pub fn append_file(path: &JsString, data: &Buffer, options: Option<AppendFileOptions>, callback: &Function);
 
     #[wasm_bindgen(js_name = "mkdtempSync")]
     pub fn mkdtemp_sync(prefix: &JsString, options: Option<MkdtempSyncOptions>) -> JsString;
