cfaca7e7fbba61207233df1a2dd1bbe412aedb72
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-10 15:59:26 -0700

    Test for fs.appendFile

--- tests/node.rs
+++ tests/node.rs
@@ -870,6 +870,17 @@ mod module {
             let mode = Default::default();
             fs::access_sync(path, mode);
         }
+
+        #[wasm_bindgen_test]
+        fn append_file() {
+            let path = &crate::helper::file::tmpfile("append_file.test");
+            let data = &crate::helper::buffer::new();
+            let options = Default::default();
+            let clo = Closure::wrap(Box::new(|| {}) as Box<dyn Fn()>);
+            let fun = clo.as_ref().unchecked_ref();
+            fs::append_file(path, data, options, fun);
+            clo.forget();
+        }
     }
 
     mod path {
