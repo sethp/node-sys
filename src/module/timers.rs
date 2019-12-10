e4be494844cf984d0b282743e1bdf6cd28823eef
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-10 08:06:38 -0700

    Binding timers.setImmediate

--- src/module/timers.rs
+++ src/module/timers.rs
@@ -1,4 +1,9 @@
+use crate::class::Immediate;
+use js_sys::Function;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(module = "timers")]
-extern {}
+extern {
+    #[wasm_bindgen(variadic, js_name = "setImmediate")]
+    pub fn set_immediate(callback: &Function, args: Box<[JsValue]>) -> Immediate;
+}
