52b8af513951386acf671e2135c72205c155e986
darinmorrison <darinmorrison@users.noreply.github.com>
2019-12-12 01:12:39 +0000

    Automated deployment: Thu Dec 12 01:12:39 UTC 2019 43e54c168174d3e66e7b687985baf0048857bc41

---
+++ implementors/core/marker/trait.Copy.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["node_sys"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"node_sys/struct.AgentConstructorOptions.html\" title=\"struct node_sys::AgentConstructorOptions\">AgentConstructorOptions</a>",synthetic:false,types:["node_sys::interface::agent_constructor_options::AgentConstructorOptions"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
