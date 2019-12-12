7c511f3a714d5d389fc597c0c7369855e97fdee6
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-11 17:11:41 -0700

    Stub for http.Server class

--- src/class/http/mod.rs
+++ src/class/http/mod.rs
@@ -1,7 +1,9 @@
 pub(crate) mod agent;
 pub(crate) mod client_request;
 pub(crate) mod incoming_message;
+pub(crate) mod server;
 
 pub use agent::*;
 pub use client_request::*;
 pub use incoming_message::*;
+pub use server::*;
