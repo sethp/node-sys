0d20788ff87c4a4c58fa87d4ddd5d2229d1d7920
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-12 08:58:13 -0700

    Stub for crypto.DiffieHellmanGroup class

---
+++ src/class/crypto/diffie_hellman_group.rs
@@ -0,0 +1,9 @@
+use js_sys::Object;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen(module = "crypto")]
+extern {
+    #[wasm_bindgen(extends = Object)]
+    #[derive(Clone, Debug, PartialEq)]
+    pub type DiffieHellmanGroup;
+}
