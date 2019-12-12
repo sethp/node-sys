b09ffd49e7cc9a2b4327f4421f3426a8a446042d
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-12 10:49:54 -0700

    Stub for worker_threads.MessageChannel class

---
+++ src/class/worker_threads/message_channel.rs
@@ -0,0 +1,9 @@
+use js_sys::Object;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen(module = "worker_threads")]
+extern {
+    #[wasm_bindgen(extends = Object)]
+    #[derive(Clone, Debug)]
+    pub type MessageChannel;
+}
