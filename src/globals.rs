63297f789ffb501611edefe9434b2ae140da4071
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-13 23:15:34 -0700

    Bindings for more global items

--- src/globals.rs
+++ src/globals.rs
@@ -1,4 +1,5 @@
-use js_sys::JsString;
+use crate::class::console::Console;
+use js_sys::{Function, JsString, Object};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -6,6 +7,19 @@ extern {
     pub static __dirname: JsString;
 
     pub static __filename: JsString;
+
+    pub static console: Console;
+
+    pub static exports: Object;
+
+    pub static global: Object;
+
+    pub static module: Object;
+
+    // pub static process: Object; // FIXME: name collision
+
+    #[wasm_bindgen(js_name = "queueMicrotask")]
+    pub fn queue_microtask(callback: &Function);
 
     pub fn require(id: &JsString) -> JsValue;
 }
