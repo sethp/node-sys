979d6bc6d45f963091263ab346ddee9b763de2c4
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-17 16:25:06 -0700

    Bindings for Timeout class

--- src/class/timeout.rs
+++ src/class/timeout.rs
@@ -7,4 +7,16 @@ extern {
     #[wasm_bindgen(extends = Object, extends = Timer)]
     #[derive(Clone, Debug, Eq, PartialEq)]
     pub type Timeout;
+
+    #[wasm_bindgen(method, js_name = "hasRef")]
+    pub fn has_ref(this: &Timeout) -> bool;
+
+    #[wasm_bindgen(method, js_name = "ref")]
+    pub fn ref_(this: &Timeout) -> Timeout;
+
+    #[wasm_bindgen(method)]
+    pub fn refresh(this: &Timeout) -> Timeout;
+
+    #[wasm_bindgen(method)]
+    pub fn unref(this: &Timeout) -> Timeout;
 }
