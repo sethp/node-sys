bad05164d5c19b228e025427ac37a4921a44ed1f
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-18 14:23:38 -0700

    Test for Console constructor

--- tests/node.rs
+++ tests/node.rs
@@ -572,6 +572,19 @@ mod class {
         }
     }
 
+    mod console {
+        use node_sys::{console, process, ConsoleConstructorOptions};
+        use wasm_bindgen_test::*;
+
+        #[wasm_bindgen_test]
+        pub fn new() {
+            let stdout = process.stdout();
+            let stderr = process.stderr();
+            let options = ConsoleConstructorOptions::new(stdout, stderr);
+            console::Console::new(options);
+        }
+    }
+
     mod hash {
         use node_sys::crypto;
         use wasm_bindgen_test::*;
