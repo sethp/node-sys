1bee0590e0622dbee77201e5ab0ad2090148b5f0
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-23 16:27:04 -0700

    Fix js_name for ECDH
    [skip ci]

--- src/class/crypto/ecdh.rs
+++ src/class/crypto/ecdh.rs
@@ -3,7 +3,7 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(module = "crypto")]
 extern {
-    #[wasm_bindgen(extends = Object)]
+    #[wasm_bindgen(extends = Object, js_name = "ECDH")]
     #[derive(Clone, Debug, PartialEq)]
     pub type Ecdh;
 }
