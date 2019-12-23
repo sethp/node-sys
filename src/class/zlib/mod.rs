1dd89ca526423b7f50cd97c80238e7a3f9f4d95a
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-23 16:06:32 -0700

    Stub for zlib.Gunzip class

--- src/class/zlib/mod.rs
+++ src/class/zlib/mod.rs
@@ -2,8 +2,10 @@ pub(crate) mod brotli_compress;
 pub(crate) mod brotli_decompress;
 pub(crate) mod deflate;
 pub(crate) mod deflate_raw;
+pub(crate) mod gunzip;
 
 pub use brotli_compress::*;
 pub use brotli_decompress::*;
 pub use deflate::*;
 pub use deflate_raw::*;
+pub use gunzip::*;
