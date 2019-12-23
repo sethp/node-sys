d216ff8c031cb4723f5833e10237fcc5532f9188
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-23 16:05:59 -0700

    Stub for zlib.DeflateRaw class

--- src/class/zlib/mod.rs
+++ src/class/zlib/mod.rs
@@ -1,7 +1,9 @@
 pub(crate) mod brotli_compress;
 pub(crate) mod brotli_decompress;
 pub(crate) mod deflate;
+pub(crate) mod deflate_raw;
 
 pub use brotli_compress::*;
 pub use brotli_decompress::*;
 pub use deflate::*;
+pub use deflate_raw::*;
