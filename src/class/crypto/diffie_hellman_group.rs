dbb6688d62435ca1be23ab248ebea13bf0da7395
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-27 09:38:07 -0700

    Set js_names for DiffieHellmanGroup

--- src/class/crypto/diffie_hellman_group.rs
+++ src/class/crypto/diffie_hellman_group.rs
@@ -19,31 +19,31 @@ extern {
         output_encoding: Option<&str>,
     ) -> JsValue;
 
-    #[wasm_bindgen(method)]
+    #[wasm_bindgen(method, js_name = "generateKeys")]
     pub fn generate_keys(this: &DiffieHellmanGroup, encoding: Option<&str>) -> JsValue;
 
-    #[wasm_bindgen(method)]
+    #[wasm_bindgen(method, js_name = "getGenerator")]
     pub fn get_generator(this: &DiffieHellmanGroup, encoding: Option<&str>) -> JsValue;
 
-    #[wasm_bindgen(method)]
+    #[wasm_bindgen(method, js_name = "getPrime")]
     pub fn get_prime(this: &DiffieHellmanGroup, encoding: Option<&str>) -> JsValue;
 
-    #[wasm_bindgen(method)]
+    #[wasm_bindgen(method, js_name = "getPrivateKey")]
     pub fn get_private_key(this: &DiffieHellmanGroup, encoding: Option<&str>) -> JsValue;
 
-    #[wasm_bindgen(method)]
+    #[wasm_bindgen(method, js_name = "getPublicKey")]
     pub fn get_public_key(this: &DiffieHellmanGroup, encoding: Option<&str>) -> JsValue;
 
-    #[wasm_bindgen(method)]
+    #[wasm_bindgen(method, js_name = "setPrivateKey")]
     pub fn set_private_key(this: &DiffieHellmanGroup, private_key: &JsValue, encoding: Option<&str>);
 
-    #[wasm_bindgen(method)]
+    #[wasm_bindgen(method, js_name = "setPublicKey")]
     pub fn set_public_key(this: &DiffieHellmanGroup, public_key: &JsValue, encoding: Option<&str>);
 
     //*********************//
     // Instance Properties //
     //*********************//
 
-    #[wasm_bindgen(method, getter)]
+    #[wasm_bindgen(method, getter, js_name = "verifyError")]
     pub fn verify_error(this: &DiffieHellmanGroup) -> u32;
 }
