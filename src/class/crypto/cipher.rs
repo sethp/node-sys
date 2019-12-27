f4cf26772599305bd4741a2998104484be3b72dc
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-26 19:05:12 -0700

    Bindings for crypto.Cipher class

--- src/class/crypto/cipher.rs
+++ src/class/crypto/cipher.rs
@@ -1,3 +1,4 @@
+use crate::{class::Buffer, interface::SetAadOptions};
 use js_sys::Object;
 use wasm_bindgen::prelude::*;
 
@@ -6,4 +7,24 @@ extern {
     #[wasm_bindgen(extends = Object)]
     #[derive(Clone, Debug)]
     pub type Cipher;
+
+    #[wasm_bindgen(method, js_name = "final")]
+    pub fn final_(this: &Cipher, output_encoding: Option<&str>) -> JsValue;
+
+    #[wasm_bindgen(method, js_name = "setAAD")]
+    pub fn set_aad(this: &Cipher, buffer: &JsValue, options: Option<SetAadOptions>) -> Cipher;
+
+    #[wasm_bindgen(method, js_name = "getAuthTag")]
+    pub fn get_auth_tag(this: &Cipher) -> Buffer;
+
+    #[wasm_bindgen(method, js_name = "getAutoPadding")]
+    pub fn get_auto_padding(this: &Cipher, auto_padding: Option<bool>) -> Cipher;
+
+    #[wasm_bindgen(method)]
+    pub fn update(
+        this: &Cipher,
+        data: &JsValue,
+        input_encoding: Option<&str>,
+        output_encoding: Option<&str>,
+    ) -> JsValue;
 }
