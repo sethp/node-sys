b557688f826362321ed3f6189923e7f97c6db7e4
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-26 18:23:28 -0700

    Bindings for crypto.Certificate class

--- src/class/crypto/certificate.rs
+++ src/class/crypto/certificate.rs
@@ -1,3 +1,4 @@
+use crate::class::Buffer;
 use js_sys::Object;
 use wasm_bindgen::prelude::*;
 
@@ -6,4 +7,13 @@ extern {
     #[wasm_bindgen(extends = Object)]
     #[derive(Clone, Debug)]
     pub type Certificate;
+
+    #[wasm_bindgen(method, js_name = "exportChallenge")]
+    pub fn export_challenge(this: &Certificate, spkac: &JsValue) -> Buffer;
+
+    #[wasm_bindgen(method, js_name = "exportPublicKey")]
+    pub fn export_public_key(this: &Certificate, spkac: &JsValue, encoding: Option<&str>) -> Buffer;
+
+    #[wasm_bindgen(method, js_name = "verifySpkac")]
+    pub fn verify_spkac(this: &Certificate, spkac: &JsValue) -> bool;
 }
