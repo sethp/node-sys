5e0566398d26f2f08a0b5235fca96e4c4e0204c2
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-27 13:52:28 -0700

    Binding for crypto.Hmac

--- src/class/crypto/hmac.rs
+++ src/class/crypto/hmac.rs
@@ -6,4 +6,10 @@ extern {
     #[wasm_bindgen(extends = Transform)]
     #[derive(Clone, Debug)]
     pub type Hmac;
+
+    #[wasm_bindgen(method)]
+    pub fn digest(this: &Hmac, encoding: Option<&str>) -> JsValue;
+
+    #[wasm_bindgen(method)]
+    pub fn update(this: &Hmac, data: &JsValue, input_encoding: Option<&str>);
 }
