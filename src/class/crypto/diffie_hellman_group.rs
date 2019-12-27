5ff8c692ccef148c2da8cce5eac08646553c612a
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-27 08:42:53 -0700

    Bindings for crypto.DiffieHellmanGroup class

--- src/class/crypto/diffie_hellman_group.rs
+++ src/class/crypto/diffie_hellman_group.rs
@@ -6,4 +6,44 @@ extern {
     #[wasm_bindgen(extends = Object)]
     #[derive(Clone, Debug)]
     pub type DiffieHellmanGroup;
+
+    //******************//
+    // Instance Methods //
+    //******************//
+
+    #[wasm_bindgen(method)]
+    pub fn compute_secret(
+        this: &DiffieHellmanGroup,
+        other_public_key: &JsValue,
+        input_encoding: Option<&str>,
+        output_encoding: Option<&str>,
+    ) -> JsValue;
+
+    #[wasm_bindgen(method)]
+    pub fn generate_keys(this: &DiffieHellmanGroup, encoding: Option<&str>) -> JsValue;
+
+    #[wasm_bindgen(method)]
+    pub fn get_generator(this: &DiffieHellmanGroup, encoding: Option<&str>) -> JsValue;
+
+    #[wasm_bindgen(method)]
+    pub fn get_prime(this: &DiffieHellmanGroup, encoding: Option<&str>) -> JsValue;
+
+    #[wasm_bindgen(method)]
+    pub fn get_private_key(this: &DiffieHellmanGroup, encoding: Option<&str>) -> JsValue;
+
+    #[wasm_bindgen(method)]
+    pub fn get_public_key(this: &DiffieHellmanGroup, encoding: Option<&str>) -> JsValue;
+
+    #[wasm_bindgen(method)]
+    pub fn set_private_key(this: &DiffieHellmanGroup, private_key: &JsValue, encoding: Option<&str>);
+
+    #[wasm_bindgen(method)]
+    pub fn set_public_key(this: &DiffieHellmanGroup, public_key: &JsValue, encoding: Option<&str>);
+
+    //*********************//
+    // Instance Properties //
+    //*********************//
+
+    #[wasm_bindgen(method, getter)]
+    pub fn verify_error(this: &DiffieHellmanGroup) -> u32;
 }
