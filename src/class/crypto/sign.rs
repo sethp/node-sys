35e6dc35ae434f4a78b79fcf40d78046432ebb24
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-27 14:17:35 -0700

    Bindings for crypto.Sign class

--- src/class/crypto/sign.rs
+++ src/class/crypto/sign.rs
@@ -6,4 +6,14 @@ extern {
     #[wasm_bindgen(extends = Writable)]
     #[derive(Clone, Debug)]
     pub type Sign;
+
+    //******************//
+    // Instance Methods //
+    //******************//
+
+    #[wasm_bindgen(method)]
+    pub fn sign(this: &Sign, private_key: &JsValue, output_encoding: Option<&str>) -> JsValue;
+
+    #[wasm_bindgen(method)]
+    pub fn update(this: &Sign, data: &JsValue, input_encoding: Option<&str>);
 }
