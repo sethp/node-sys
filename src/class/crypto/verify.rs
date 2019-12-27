a0cabef047843be487732eb54a483067b02d5dfc
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-12-27 14:21:02 -0700

    Bindings for crypto.Verify class

--- src/class/crypto/verify.rs
+++ src/class/crypto/verify.rs
@@ -6,4 +6,10 @@ extern {
     #[wasm_bindgen(extends = Writable)]
     #[derive(Clone, Debug)]
     pub type Verify;
+
+    #[wasm_bindgen(method)]
+    pub fn update(this: &Verify, data: &JsValue, input_encoding: Option<&str>);
+
+    #[wasm_bindgen(method)]
+    pub fn verify(this: &Verify, object: &JsValue, signature: &JsValue, signature_encoding: Option<&str>) -> bool;
 }
