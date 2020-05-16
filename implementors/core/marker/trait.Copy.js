92dc8565794898b6f9cb7227130c3cfd478cbbf7
darinmorrison <darinmorrison@users.noreply.github.com>
2020-05-16 05:22:00 +0000

    Automated deployment: Sat May 16 05:22:00 UTC 2020 6b6248e465a26a01ea1fef5f162873c1a46cc4b4

--- implementors/core/marker/trait.Copy.js
+++ implementors/core/marker/trait.Copy.js
@@ -1,9 +1,3 @@
 (function() {var implementors = {};
-implementors["node_sys"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"node_sys/struct.NetServerOptions.html\" title=\"struct node_sys::NetServerOptions\">NetServerOptions</a>",synthetic:false,types:["node_sys::interface::net_server_options::NetServerOptions"]},];
-
-            if (window.register_implementors) {
-                window.register_implementors(implementors);
-            } else {
-                window.pending_implementors = implementors;
-            }
-        })()
+implementors["node_sys"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"node_sys/struct.NetServerOptions.html\" title=\"struct node_sys::NetServerOptions\">NetServerOptions</a>","synthetic":false,"types":["node_sys::interface::net_server_options::NetServerOptions"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
