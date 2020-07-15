75608569c145a6e6bf149106291f5a5fdc2b68c3
Seth Pellegrino <seth@codecopse.net>
2020-07-15 15:23:53 -0700

    feat: readFileSync bindings

--- src/module/fs.rs
+++ src/module/fs.rs
@@ -1,19 +1,31 @@
 pub use crate::{
     class::{fs::*, Buffer},
-    interface::{AppendFileOptions, MkdtempSyncOptions, WriteFileSyncOptions},
+    interface::{AppendFileOptions, MkdtempSyncOptions, ReadFileSyncOptions, WriteFileSyncOptions},
 };
 use js_sys::{Function, JsString};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(module = "fs")]
-extern {
+extern "C" {
     pub fn access(path: &JsString, mode: Option<u32>, callback: &Function);
 
     #[wasm_bindgen(js_name = "accessSync")]
     pub fn access_sync(path: &JsString, mode: Option<u32>);
 
     #[wasm_bindgen(js_name = "appendFile")]
-    pub fn append_file(path: &JsString, data: &Buffer, options: Option<AppendFileOptions>, callback: &Function);
+    pub fn append_file(
+        path: &JsString,
+        data: &Buffer,
+        options: Option<AppendFileOptions>,
+        callback: &Function,
+    );
+
+    // TODO: gets back a JsString if encoding is passed
+    #[wasm_bindgen(js_name = "readFileSync", catch)]
+    pub fn read_file_sync(
+        path: &JsString,
+        options: Option<ReadFileSyncOptions>,
+    ) -> Result<Buffer, JsValue>;
 
     #[wasm_bindgen(js_name = "mkdtempSync")]
     pub fn mkdtemp_sync(prefix: &JsString, options: Option<MkdtempSyncOptions>) -> JsString;
